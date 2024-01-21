d2b86c2126deac4dd1ce175bf24f751108fb1eed
Traven Wells <104939327+WellsTraven-FS@users.noreply.github.com>
2024-01-20 20:10:20 -0500

    build

--- api/server.js
+++ api/server.js
@@ -35,7 +35,7 @@ app.use(express.static(buildPath));
 // For any routes not defined by the api, assume it's a direct request to a client-side route
 app.get("*", (req, res) => {
     res.sendFile(
-        path.join(__dirname, "../prominentapp/build/index.html"),
+        path.join(__dirname, "../prominentapp/build", "index.html"),
         function (err) {
             if (err) {
                 res.status(500).send(err);
